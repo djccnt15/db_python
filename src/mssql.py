a8810c3ef5a25fb58e76354897be4f2878c73e7e
djccnt15 <djccnt15@gmail.com>
2023-07-18 18:29:12 +0900

    create db_commit func

--- src/mssql.py
+++ src/mssql.py
@@ -7,3 +7,10 @@ def read(db: Dict, query: str):
         with conn.cursor(as_dict=True) as cursor:
             cursor.execute(query)
             return [row for row in cursor]
+
+
+def db_commit(query: str, host: str, port: str, user: str, pw: str, db: str):
+    with pymssql.connect(f'{host}:{port}', user, pw, db) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(query)
+            conn.commit()
